fbce92a21ca7573e69bd5edd49483e955c924729
= <26445308+GingaaBread@users.noreply.github.com>
2023-07-15 15:36:37 +0200

    Scroll Reveal

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -5,3 +5,15 @@ window.addEventListener("scroll", () => {
     topChevron.classList.remove("hidden");
   else topChevron.classList.add("hidden");
 });
+
+ScrollReveal({
+  origin: "left",
+  distance: "5rem",
+  reset: true,
+}).reveal(".project .text", { delay: 200 });
+
+ScrollReveal({
+  origin: "right",
+  distance: "5rem",
+  reset: true,
+}).reveal(".project .media", { delay: 100 });
